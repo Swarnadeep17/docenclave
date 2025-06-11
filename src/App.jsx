5a81fba5676395218800b8049af316cee8d99fab
Swarnadeep17 <diip.brdhn@gmail.com>
2025-06-11 20:02:27 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Layout from './components/layout/Layout.jsx'
 import Home from './pages/Home.jsx'
 import PDFMerge from './tools/pdf/merge/PDFMerge.jsx'
+import PDFSplit from './tools/pdf/split/PDFSplit.jsx'
 
 function App() {
   return (
@@ -11,6 +12,10 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/tools/pdf/merge" element={<PDFMerge />} />
+          <Route path="/tools/pdf/split" element={<PDFSplit />} />
+          {/* Future routes */}
+          {/* <Route path="/tools/pdf/compress" element={<PDFCompress />} /> */}
+          {/* <Route path="/tools/image/resize" element={<ImageResize />} /> */}
         </Routes>
       </Layout>
     </Router>
