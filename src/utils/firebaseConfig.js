0c090db3ca752735628f5e4bc09f134687816fdd
Swarnadeep17 <diip.brdhn@gmail.com>
2025-06-11 16:42:59 +0530

    Create src/utils/firebaseConfig.js

---
+++ src/utils/firebaseConfig.js
@@ -0,0 +1,10 @@
+// Firebase config for client-side access
+// This is safe to expose publicly
+export const firebaseConfig = {
+  apiKey: "AIzaSyAwgqYEEUWu0aGminZCl11c_yKYfUu-9MU", // You can get this from Firebase Console
+  authDomain: "docenclave-d5a43.firebaseapp.com",
+  projectId: "docenclave-d5a43",
+  storageBucket: "docenclave-d5a43.firebasestorage.app",
+  messagingSenderId: "13497976521",
+  appId: "1:13497976521:web:fd2f8c357e3bdebfaf6f18" // You can get this from Firebase Console
+}
