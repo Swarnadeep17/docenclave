ced54082241b54f570ff3382233f5f4e126d3e13
Swarnadeep17 <diip.brdhn@gmail.com>
2025-06-11 15:43:18 +0530

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  
-  // This ensures links are generated as /pdf-tools/ which works perfectly with Firebase Hosting's static file serving.
-  trailingSlash: true,
-};
-
-export default nextConfig;
