2428bd6ff670509331945a18104a02daf9ac33ab
Swarnadeep17 <diip.brdhn@gmail.com>
2025-06-20 22:49:49 +0530

    Delete src directory

--- src/lib/statsManager.js
+++
@@ -1,9 +0,0 @@
-import { db } from "./firebase.js";
-import { ref, runTransaction } from "firebase/database";
-
-export async function incrementStat(pathParts) {
-    const statRef = ref(db, `/stats/${pathParts.join("/")}`);
-    return runTransaction(statRef, (currentVal) => {
-        return (currentVal || 0) + 1;
-    });
-}
